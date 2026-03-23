21cb732151e5d5beda1518484ed7cecb9eaf9ea8
Pegoral123 <jhonathanpegoral123@gmail.com>
2026-03-23 18:54:35 -0300

    Sintaxe da List

--- EstudosDeC#.Common/Main.cs
+++ EstudosDeC#.Common/Main.cs
@@ -1,33 +1,87 @@
+List<string> nameUser = new List<string>();
 
-Console.WriteLine("========CRIANDO E PERCORRENDO ARRAY========");
+nameUser.Add("Jhonathan");
+nameUser.Add("Larissa");
 
-int[] numBer = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-foreach (int i in numBer)
+foreach (string i in nameUser)
 {
     Console.WriteLine(i);
 }
-Console.WriteLine("========REDIMENSIONANDO ARRAY E PREENCHENDO ========");
 
-Array.Resize(ref numBer, 11);
-numBer[10] = 11;
 
-for (int i = 0; i < numBer.Length; i++)
-{
-    Console.WriteLine(numBer[i]);
-}
 
-Console.WriteLine("========COPIANDO ARRAY E PREENCHENDO ========");
 
-int[] numBerCopiado = new int[numBer.Length * 2];
-Array.Copy(numBer, numBerCopiado, numBer.Length);
 
-numBerCopiado[10] = 11;
-numBerCopiado[11] = 12;
-numBerCopiado[12] = 13;
-numBerCopiado[13] = 14;
-numBerCopiado[14] = 15;
 
-foreach (int i in numBerCopiado)
-{
-    Console.WriteLine(i);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Console.WriteLine("========CRIANDO E PERCORRENDO ARRAY========");
+
+// int[] numBer = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+// foreach (int i in numBer)
+// {
+//     Console.WriteLine(i);
+// }
+// Console.WriteLine("========REDIMENSIONANDO ARRAY E PREENCHENDO ========");
+
+// Array.Resize(ref numBer, 11);
+// numBer[10] = 11;
+
+// for (int i = 0; i < numBer.Length; i++)
+// {
+//     Console.WriteLine(numBer[i]);
+// }
+
+// Console.WriteLine("========COPIANDO ARRAY E PREENCHENDO ========");
+
+// int[] numBerCopiado = new int[numBer.Length * 2];
+// Array.Copy(numBer, numBerCopiado, numBer.Length);
+
+// numBerCopiado[10] = 11;
+// numBerCopiado[11] = 12;
+// numBerCopiado[12] = 13;
+// numBerCopiado[13] = 14;
+// numBerCopiado[14] = 15;
+
+// foreach (int i in numBerCopiado)
+// {
+//     Console.WriteLine(i);
+// }
